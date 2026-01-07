ac9cc6229576cff9c8117b84bc4b32f2688699cb
rianandnan <rianandnan@gmail.com>
2026-01-07 21:37:37 +0530

    Add new star pattern

---
+++ patterns5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class patterns4 {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) { // rows
+            for (int j = 1; j <= n - i; j++) { // spaces
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) { // stars
+                System.out.print("*");
+            }
+            System.out.println(); // new line
+        }
+    }
+}
