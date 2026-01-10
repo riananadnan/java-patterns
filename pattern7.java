e74eee36762059019008f5e0e618d3d990f6c56a
rianandnan <riananadnan@gmail.com>
2026-01-10 21:39:25 +0530

    prints till 5

--- pattern7.java
+++ pattern7.java
@@ -4,8 +4,8 @@ public class pattern7 {
     public static void main(String args[]) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
