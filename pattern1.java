29108ac3a3c451c8161abb382a839739f75bdac6
rianandnan <riananadnan@gmail.com>
2026-01-11 21:40:25 +0530

    pattern 1 Solid  Rectangle

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+public class pattern1 {
+    public static void main(String[] args) {
+
+        int rows = 4;
+        int cols = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= cols; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
